7425e1f512dfde8af8fa659f19b02feedfaf8062
shristi-bhushan <shristibhushan2001@gmail.com>
2023-04-25 23:06:36 +0530

    App code

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import NoteArea from './components/NoteArea';
 import Header from "./components/Header";
 import React, {useState} from 'react';
+import NewNote from './components/NewNote';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -15,10 +17,31 @@ function App() {
     console.log(notes);
   }
 
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index!==id;
+      });
+    });
+  }
+
   return (
     <div className="App">
       <Header />
       <NoteArea onAdd={addNote}/>
+      {notes.map( (noteItem, index) => {
+        return (
+             <NewNote 
+                key = {index}
+                id = {index}
+                title={noteItem.title}
+                content={noteItem.content}
+                onDelete={deleteNote}
+        />
+        );
+      } )}
+      <Footer />
+      
     </div>
   ); 
 }
